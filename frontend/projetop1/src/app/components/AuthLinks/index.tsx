63c2cac8421e131d9dacf37d6ebc2d5699de5aed
João Juvino <joao.pedro.santos@ccc.ufcg.edu.br>
2025-07-14 15:54:50 -0300

    feat: adding component for the following links: forgot password and register

---
+++ frontend/projetop1/src/app/components/AuthLinks/index.tsx
@@ -0,0 +1,10 @@
+const AuthLinks = () => {
+    return (
+        <>
+            <a className="text-[#37474F]" href="#">Esqueceu a senha?</a>
+            <a className="text-[#37474F]" href="#">Cadastre-se</a>
+        </>
+    );
+}
+
+export default AuthLinks;
