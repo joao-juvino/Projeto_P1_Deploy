ff551e104debf75895a35b5a56569d9f609fce82
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2025-07-20 15:37:21 -0300

    feature: add authentication routes blueprint to app initialization

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,22 +2,28 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from app.database.mongo_client import get_database, close_database_connection
+from app.routes.auth_routes import auth_bp
 
 load_dotenv()
 
-# TODO: USE LOGGER TO PROVIDE INFORMATION ABOUT APP EXECUTION
 
 def create_app():
     app = Flask(__name__)
 
+    # Establish connection with MongoDB
     with app.app_context():
         try:
             app.mongo_db = get_database()
         except Exception as e:
             raise RuntimeError(f"Application failed to start due to MongoDB connection error: {e}")
     
+    # Register application blueprints
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+
     @app.teardown_appcontext
     def teardown_db_connection(exception=None):
         close_database_connection()
+
 
     return app
