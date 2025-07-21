8980d05249e08a5791196d6109aa85668612ac0e
Pedro Henrique Sarmento Pereira <pedro.pereira@ccc.ufcg.edu.br>
2025-07-20 23:29:17 -0300

    adição de endpoint para confirmação de email

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,24 +1,44 @@
 from flask import Blueprint, request, jsonify, current_app
 from app.services import auth_service
-from app.exceptions.auth_exceptions import UserAlreadyExistsError
+from app.exceptions.auth_exceptions import UsernameAlreadyTakenError, EmailAlreadyRegisteredError
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.route("/signup", methods=["POST"])
 def signup_route():
     data = request.get_json()
-    username = data.get("username")
-    email = data.get("email")
-    password = data.get("password")
-
-    database = current_app.mongo_db
 
     try:
-        user_id = auth_service.register_user(database, username, email, password)
-        return jsonify({"message": "Successfully registered user!", "user_id": user_id}), 201
-    except UserAlreadyExistsError as e:
+        auth_service.register_user(
+            db_client=current_app.mongo_db,
+            username=data.get("username"),
+            email=data.get("email"),
+            password=data.get("password")
+        )
+        return jsonify({
+            "message": "Registration request successful! Please check your email to confirm your account."
+        }), 202
+
+    except (UsernameAlreadyTakenError, EmailAlreadyRegisteredError) as e:
         return jsonify({"message": e.message}), 409
+
     except ValueError as e:
-        return jsonify({"message": e.message}), 400
+        return jsonify({"message": str(e)}), 400
+
     except Exception as e:
-        return jsonify({"message": "Interal error occurred while registering user."}), 500
+        return jsonify({"message": "An internal server error occurred."}), 500
+    
+
+@auth_bp.route("/confirm/<token>", methods=["GET"])
+def confirm_email_route(token: str):
+    db_client = current_app.mongo_db
+
+    try:
+        auth_service.confirm_user_token(db_client, token)
+        return jsonify({"message": "E-mail confirmado com sucesso! Você já pode fazer login."}), 200
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
+
+    except Exception as e:
+        return jsonify({"message": "Ocorreu um erro interno ao processar a confirmação."}), 500
