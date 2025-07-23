930ac5a01b733354bd34c796ff506de7d763b7c6
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2025-07-22 21:06:33 -0300

    [add] added login endpoint

--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, request, jsonify, current_app
 from app.services import auth_service
-from app.exceptions.auth_exceptions import UsernameAlreadyTakenError, EmailAlreadyRegisteredError
+from app.exceptions.auth_exceptions import (
+    UsernameAlreadyTakenError, 
+    EmailAlreadyRegisteredError, 
+    InvalidCredentialsError
+)
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -17,14 +21,12 @@ def signup_route():
         )
         return jsonify({
             "message": "Registration request successful! Please check your email to confirm your account."
-        }), 202
+        }), 201
 
     except (UsernameAlreadyTakenError, EmailAlreadyRegisteredError) as e:
         return jsonify({"message": e.message}), 409
-
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
-
     except Exception as e:
         return jsonify({"message": "An internal server error occurred."}), 500
     
@@ -39,5 +41,22 @@ def confirm_email_route(token: str):
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
 
+    except Exception as e:
+        return jsonify({"message": "An internal error occurred while processing the confirmation."}), 500
+
+@auth_bp.route("/signin", methods=["POST"])
+def login():
+    database = current_app.mongo_db
+    
+    data = request.json
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    try:
+        auth_service.login(database, username, email, password)
+        return jsonify({"message": "Login Successful!"}), 200
+    except InvalidCredentialsError as e:
+        return jsonify({"message": e.message()}), 401
     except Exception as e:
         return jsonify({"message": "An internal error occurred while processing the confirmation."}), 500
