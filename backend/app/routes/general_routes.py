64a07c301bfc6dd80070659b33fb78215529fc09
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2025-08-05 22:44:09 -0300

    [update] migrate general routes from Flask to Sanic

--- backend/app/routes/general_routes.py
+++ backend/app/routes/general_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from sanic import Blueprint, Request
+from sanic.response import json
 
 general_bp = Blueprint("general", __name__)
 
 @general_bp.route("/", methods=["GET"])
-def index():
-    return jsonify({"message:": "Server is running!"}), 200
+async def index():
+    return json({"message": "Gotta go fast!"}, status=200)
